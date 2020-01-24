30a4fea7ed0bf9822b543716bf2db6e32912b1c6
Francesc Arbona <francesc.arbona@capside.com>
2020-01-24 14:31:30 +0000

    Implement fetch and worker functions

--- assault/http.py
+++ assault/http.py
@@ -1,17 +1,28 @@
 import asyncio
+import os
 import time
+import requests
 
 
 def fetch(url):
     """Make the request and get the results"""
-    pass
+    started_at = time.monotonic()
+    response = requests.get(url)
+    request_time = time.monotonic() - started_at
+    return {"status_code": response.status_code, "request_time": request_time}
 
 
 async def worker(name, queue, results):
-    """A function to take unmake requests from a queue and perform the work then add resutls to the resutls list"""
+    """A function to take unmake requests from a queue and perform the work then add results to the resutls list"""
+    loop = asyncio.get_event_loop()
     while True:
-        item = await queue.get()
-        print(f"{name} read {item}")
+        url = await queue.get()
+        if os.getenv("DEBUG"):
+            print(f"{name} - Fetching {url}")
+        future_result = loop.run_in_executor(None, fetch, url)
+        result = await future_result
+        results.append(result)
+        queue.task_done()
 
 
 async def distribute_work(url, requests, concurrency, results):
