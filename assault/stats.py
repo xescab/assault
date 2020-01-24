032a1f1a76b5f3b33e8a8949a381e477a70494ca
Francesc Arbona <francesc.arbona@capside.com>
2020-01-24 15:16:39 +0000

    Use lambdas and list comprehensions

--- assault/stats.py
+++ assault/stats.py
@@ -1,4 +1,5 @@
 from typing import List, Dict
+from statistics import mean
 
 
 class Results:
@@ -17,7 +18,7 @@ class Results:
 
     def __init__(self, total_time: float, requests: List[Dict]):
         self.total_time = total_time
-        self.requests = requests
+        self.requests = sorted(requests, key=lambda r: r["request_time"])
 
     def slowest(self) -> float:
         """
@@ -36,11 +37,7 @@ class Results:
         >>> results.slowest()
         6.1
         """
-        slowest_request_time = 0.0
-        for request in self.requests:
-            if request["request_time"] > slowest_request_time:
-                slowest_request_time = request["request_time"]
-        return slowest_request_time
+        return self.requests[-1]["request_time"]
 
     def fastest(self) -> float:
         """
@@ -48,22 +45,18 @@ class Results:
 
         >>> results = Results(10.6, [{
         ...     'status_code': 200,
-        ...     'request_time': 3.4
+        ...     'request_time': 1.04
         ... }, {
         ...     'status_code': 500,
         ...     'request_time': 6.1
         ... }, {
         ...     'status_code': 200,
-        ...     'request_time': 1.04
+        ...     'request_time': 3.4
         ... }])
         >>> results.fastest()
         1.04
         """
-        fastest_request_time = 1000.0
-        for request in self.requests:
-            if request["request_time"] < fastest_request_time:
-                fastest_request_time = request["request_time"]
-        return fastest_request_time
+        return self.requests[0]["request_time"]
 
     def average_time(self) -> float:
         """
@@ -80,9 +73,9 @@ class Results:
         ...     'request_time': 1.04
         ... }])
         >>> results.average_time()
-        3.533333333333333
+        3.513333333333333
         """
-        return self.total_time / len(self.requests)
+        return mean([r["request_time"] for r in self.requests])
 
     def total_time(self) -> float:
         pass
@@ -104,9 +97,5 @@ class Results:
         >>> results.successful_requests()
         2
         """
-        count = 0
-        for request in self.requests:
-            if request["status_code"] == 200:
-                count = count + 1
-        return count
+        return len([r for r in self.requests if r["status_code"] in range(200, 299)])
 
