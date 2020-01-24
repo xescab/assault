646f3d274a2b984141a7a83d8a4d586cd231d7e4
Francesc Arbona <francesc.arbona@capside.com>
2020-01-24 14:45:29 +0000

    Add tests

--- assault/stats.py
+++ assault/stats.py
@@ -20,16 +20,80 @@ class Results:
         self.requests = requests
 
     def slowest(self) -> float:
+        """
+        Returns the slowest request's completion time
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04
+        ... }])
+        >>> results.slowest()
+        6.1
+        """
         pass
 
     def fastest(self) -> float:
+        """
+        Returns the fastest request's completion time
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04
+        ... }])
+        >>> results.fastest()
+        1.04
+        """
         pass
 
     def average_time(self) -> float:
+        """
+        Returns the average request completion time
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04
+        ... }])
+        >>> results.average_time()
+        9.846666667
+        """
         pass
 
     def total_time(self) -> float:
         pass
 
     def successful_requests(self) -> int:
+        """
+        Returns the number of successful requests (return code 200)
+
+        >>> results = Results(10.6, [{
+        ...     'status_code': 200,
+        ...     'request_time': 3.4
+        ... }, {
+        ...     'status_code': 500,
+        ...     'request_time': 6.1
+        ... }, {
+        ...     'status_code': 200,
+        ...     'request_time': 1.04
+        ... }])
+        >>> results.successful_requests()
+        2
+        """
         pass
